65d98ae8ed2edcc528a8048c4e49adf3271b8280
tomasemilio <tsilvaebensperger@gmail.com>
2022-08-15 15:49:15 -0400

    push

--- lua/colorscheme_custom.lua
+++ lua/colorscheme_custom.lua
@@ -1,8 +1,11 @@
 -- Example config in Lua
 vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+vim.g.tokyonight_sidebars = {}
 vim.g.tokyonight_transparent = true
+vim.g.tokyonight_terminal_colors = false
+vim.g.tokyonight_dark_float = false
+vim.g.tokyonight_dark_sidebar = false
 
 -- Change the "hint" color to the "orange" color, and make the "error" color bright red
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
