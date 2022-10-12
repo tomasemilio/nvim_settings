6aa9937912ec8ef0a58d37a5a1945453fbcf68bd
tomasemilio <tsilvaebensperger@gmail.com>
2022-10-12 17:32:37 -0600

    push

--- lua/telescope_custom.lua
+++ lua/telescope_custom.lua
@@ -13,7 +13,7 @@ require('telescope').setup {
                                        -- the default case_mode is "smart_case"
     },
 	file_browser = {
-		  theme = "ivy",
+		  -- theme = "dropdown",
 		  -- disables netrw and use telescope-file-browser in its place
 		  hijack_netrw = true,
 		  mappings = {
